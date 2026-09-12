2ad378cb872fdcd7e54697a3dd0374b8a2cef4e3
ShivCodes911 <workforshiv09@gmail.com>
2026-09-13 00:01:10 +0530

    feat:Signup endpoint added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
  import express from "express";
+
+ import authRouter from "./modules/auth/auth.routes.js"
  const app = express();
 
  app.use(express.json());
+
+ app.use("/api/v1/auth",authRouter);
 
  export default app;
 
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,113 @@
+import type{ Request, Response } from "express";
+import userModel from "../../models/user.model.js";
+import bcrypt from "bcrypt";
+
+
+export const signup=async(req:Request,res:Response)=>{
+    try {
+
+        const {username,password} =req.body;
+
+        const existingUser=await userModel.findOne({username});
+        if(existingUser){
+            return res.status(403).json({
+                message:"user already exists"
+            }) 
+        }
+
+
+        const hashPassword= await bcrypt.hash(password,10);
+
+        const user=await userModel.create({
+            username:username,
+            password:hashPassword
+        });
+
+        return res.status(200).json({
+            message:"User created Successfully",
+            user
+        });
+    
+    } catch (error) {
+        return res.status(500).json({
+            message:`SERVER FAILED :- ${console.error(error)}`
+        });
+    }
+
+};
+
+
+export const signin=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
+
+export const content=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
+
+export const fetchContent=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
+export const deleteContent=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
+export const shareContent=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
+export const fetchSharedContent=async(req:Request,res:Response)=>{
+    try {
+
+        
+
+        
+    } catch (error) {
+        console.error("failed",error);
+        
+    }
+
+};
