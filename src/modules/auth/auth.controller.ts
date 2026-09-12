871ca8a2be0dc79ad8f821dfb088518ae3c400dc
ShivCodes911 <workforshiv09@gmail.com>
2026-09-13 00:26:24 +0530

    feat:signin endpoint completed

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import type{ Request, Response } from "express";
+
 import userModel from "../../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+import type{ Request, Response } from "express";
 
 
 export const signup=async(req:Request,res:Response)=>{
@@ -39,12 +42,38 @@ export const signup=async(req:Request,res:Response)=>{
 
 export const signin=async(req:Request,res:Response)=>{
     try {
-
+        const {username,password}=req.body;
+        
+        const existingUser= await userModel.findOne({
+            username
+            
+        });
         
 
-        
+        if(!existingUser){
+            return res.status(404).json({
+                message:"User not found"
+            })
+        };
+
+        const matchedPassword=await bcrypt.compare(password,existingUser.password);
+
+        if(!matchedPassword){
+            return res.status(403).json({
+                message:"invalid password"
+            })
+        }
+
+        const token=jwt.sign({id:existingUser._id},process.env.JWT_SECRET!);
+
+        return res.status(200).json({
+            message:"User signed in Successfully!!!",
+            token:token,
+        })
     } catch (error) {
-        console.error("failed",error);
+         return res.status(500).json({
+            message:`SERVER FAILED :- ${console.error(error)}`
+        });
         
     }
 
